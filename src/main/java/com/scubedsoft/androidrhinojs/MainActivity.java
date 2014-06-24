26a207ef36ff3c2b169d14c2dbc223bebaa145f2
Hubert Lee <hubert.k.lee@gmail.com>
2014-06-24 17:57:21 -0400

    add basic output behavior

--- src/main/java/com/scubedsoft/androidrhinojs/MainActivity.java
+++ src/main/java/com/scubedsoft/androidrhinojs/MainActivity.java
@@ -2,14 +2,34 @@ package com.scubedsoft.androidrhinojs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    private EditText mTextInput;
+    private TextView mTextOutput;
+    private Button mSubmitButton;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTextInput = (EditText)findViewById(R.id.text_input);
+        mTextOutput = (TextView)findViewById(R.id.text_output);
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+    }
+
+    public void executeJs(View v) {
+        mTextOutput.setText(String.format("%s%s\n",
+                mTextOutput.getText(), mTextInput.getText()));
+        mTextInput.setText(null);
     }
 }
