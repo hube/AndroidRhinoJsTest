0c3c9ec210f28c92eb4605bb727d500c1fcdca0b
Hubert Lee <hubert.k.lee@gmail.com>
2014-06-24 19:16:29 -0400

    add comments

--- src/main/java/com/scubedsoft/androidrhinojs/MainActivity.java
+++ src/main/java/com/scubedsoft/androidrhinojs/MainActivity.java
@@ -32,19 +32,36 @@ public class MainActivity extends Activity
         mSubmitButton = (Button)findViewById(R.id.submit_button);
     }
 
+    /**
+     * Executes the JavaScript entered into the EditText view.
+     *
+     * Implementation instructions adapted from:
+     *   https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino/Embedding_tutorial
+    */
     public void executeJs(View v) {
+        // Retrieve text from the EditView, then clear it out
         String cmd = mTextInput.getText().toString();
         mTextOutput.setText(String.format("%s%s\n", mTextOutput.getText(), cmd));
         mTextInput.setText(null);
 
         try {
+            // Create and enter a JavaScript Context
             mJsContext = Context.enter();
+
+            // Disable optimizations. This is necessary or else we get this exception:
+            //   java.lang.UnsupportedOperationException: can't load this type of class file
             mJsContext.setOptimizationLevel(-1);
+
+            // This invocation of initStandardObjects allows us to re-use the existing mScope object
             mScope = mJsContext.initStandardObjects(mScope, false);
 
-            Object result = mJsContext.evaluateString(mScope, cmd, "MainActivity.java", 45, null);
+            // Evaluate the JavaScript and save the result
+            Object result = mJsContext.evaluateString(mScope, cmd, "MainActivity.java", 52, null);
+
+            // Output the result to the TextView
             mTextOutput.setText(String.format("%s%s\n", mTextOutput.getText(), result.toString()));
         } finally {
+            // Ensure that we release the resources consumed by the Context
             Context.exit();
         }
     }
